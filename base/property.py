e5ba4f6a6d77f551d47b12ef7da58253705ef33c
ycwfmelt <ycwfmelt@live.com>
2018-06-28 11:14:17 +0800

    decorator property

---
+++ base/property.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+class Screen(object):
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        self.__height = value
+
+    @property
+    def resolution(self):
+        return self.__height * self.__width
+
+
+# 测试:
+s = Screen()
+s.width = 1024
+s.height = 768
+print('resolution =', s.resolution)
+if s.resolution == 786432:
+    print('测试通过!')
+else:
+    print('测试失败!')
